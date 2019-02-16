c9aae112ae675788d56bab47186b9a5a46da53d9
Sherri Li <32345806+shli17@users.noreply.github.com>
2019-02-16 14:29:48 -0800

    Update script.js
    comments

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
+// The backend that uses Wikipedia's Query API to search all articles pertaining to a certain topic
+
 $( 'button' ).on( 'click' , function () {
     var searchTerm = $( 'input' ).val();
-
+	// API call
 	var wikiURL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&srsearch=' + searchTerm;
 	
 	$.ajax( {
 		url: wikiURL, 
 		dataType: "jsonp",
 		success: function( data ) {
+			// store the query results into a variable
 		    var results = data.query.search;
-		    
+		    	// display the resulting query as a JSON in the developer console
 		    console.log(results);
 		    
 		    for( var i = 0; i< results.length; i++ ) {
 		        
 		        var titletext = results[i]['title'];
-		        
+		        // Make a hyperlink to the article
 		        $( 'body' ).append("<br><a href='"+ "https://en.wikipedia.org/wiki/"+titletext.split(' ').join('_') + "'> "+titletext+" </a>");
-		        
+		        // Display the time of the most recent edit to each article
 		        $( 'body' ).append("<h4> latest edit: <small>" + results[i]['timestamp'] + "</small></h4>");
 		        
 		    }
